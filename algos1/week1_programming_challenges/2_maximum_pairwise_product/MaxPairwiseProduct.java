3d13a4a87e2e1642d6e21aeba10395c9f88a62fa
Adrien Atallah <AdrienAtallah@gmail.com>
2018-08-25 19:22:25 -0400

    add gitignore, import for max function

--- algos1/week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
+++ algos1/week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.lang.Math;
 
 public class MaxPairwiseProduct {
     static int getMaxPairwiseProduct(int[] numbers) {
@@ -8,8 +9,7 @@ public class MaxPairwiseProduct {
 
         for (int first = 0; first < n; ++first) {
             for (int second = first + 1; second < n; ++second) {
-                max_product = max(max_product,
-                    numbers[first] * numbers[second])
+                max_product = Math.max(max_product, numbers[first] * numbers[second]);
             }
         }
 
