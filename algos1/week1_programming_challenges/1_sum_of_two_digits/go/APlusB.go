d420de0b889445e2038d1b8e635eb2d7ea14c568
Adrien Atallah <AdrienAtallah@gmail.com>
2018-08-25 18:55:36 -0400

    add go solution

---
+++ algos1/week1_programming_challenges/1_sum_of_two_digits/go/APlusB.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("weeeoooo")
+
+	var a int
+	var b int
+
+	_, _ = fmt.Scanf("%d", &a)
+	_, _ = fmt.Scanf("%d", &b)
+
+	fmt.Println(a + b)
+}
